d6cd6dcc06945b54e73349ba1f1cb90f5002f800
Megameneer <dennis.hh@hotmail.com>
2018-11-16 10:54:06 +0100

    Boottoevoegscherm is aangemaakt. De plannergrid past zich aan aan de datum

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var boats = new SampleBoatController().GetBoats();
+            var reservations = new SampleReservationController().GetReservations();
+            addBoatTypeTabs(boats, reservations);
+        }
+
+        private void addBoatTypeTabs(List<Boat> boats, List<Reservation> reservations)
+        {
+            foreach (var boatType in GetDifferentBoatTypes(boats))
+            {
+                var reservationsForBoatType = GetReservationsForBoatType(reservations, boatType);
+                BoatTypeTabControl.Items.Add(new BoatTypeTabItem(boatType, reservationsForBoatType.ToList()));
+            }
+        }
+        private IEnumerable<String> GetDifferentBoatTypes(List<Boat> boats) =>
+            boats.Select(boat => boat.Type).Distinct();
+
+        private IEnumerable<Reservation> GetReservationsForBoatType(List<Reservation> reservations, string boatType) =>
+            reservations.Where(r => r.Boat.Type.Equals(boatType));
+    }
+}
---
+++ WpfApp6/SampleBoatController.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace WpfApp6
+{
+    public class SampleBoatController
+    {
+        public List<Boat> GetBoats()
+        {
+            return new List<Boat>
+            {
+                new Boat(1,"CRX", "Scull", 30, 1, false),
+                new Boat(2, "MR2", "Skiff", 60, 4, true)
+            };
+        }
+    }
+}
