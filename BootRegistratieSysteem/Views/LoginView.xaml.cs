29245bcf706c59fd907130080a37acf6a2e9fbdb
Omar Mazher <S1113954@student.windesheim.nl>
2018-11-20 15:08:08 +0100

    Add LoginController
    MVC Update

--- BootRegistratieSysteem/Views/LoginView.xaml.cs
+++ BootRegistratieSysteem/Views/LoginView.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using BootRegistratieSysteem.Views;
+using BootRegistratieSysteem.Controller;
 
 namespace BootRegistratieSysteem
 {
@@ -29,75 +30,12 @@ namespace BootRegistratieSysteem
 
         private void LoginButton(object sender, RoutedEventArgs e)
         {
-            DataBaseController u = new DataBaseController();
-            
-            using (BootDataBase context = new BootDataBase())
+            if (LoginController.Login(Username, Password, LoginError))
             {
-                var Result = context.Users.ToList();
-                if (Result.Count > 0)
-                {
-                    foreach (var results in Result)
-                    {
-                        Console.WriteLine(results);
-                        string hashedPassword = u.PasswordHash(Password.Password);
-
-                        Username.BorderBrush = Brushes.Gray;
-                        Password.BorderBrush = Brushes.Gray;
-                        Username.BorderThickness = new Thickness(1);
-                        Password.BorderThickness = new Thickness(1);
-                        LoginError.Content = "";
-                        LoginError.UpdateLayout();
-                        Username.UpdateLayout();
-                        Password.UpdateLayout();
-
-                        if (Username.Text != "")
-                        {
-                            if (results.PersonID.Equals(int.Parse(Username.Text)))
-                            {
-                                if (Password.Password != "")
-                                {
-                                    if (results.Password.Equals(hashedPassword))
-                                    {
-                                      
-                                        Switcher.Switch(new DashboardView());
-                                        LoginError.UpdateLayout();
-                                    }
-
-                                    else
-                                    {
-
-                                        Password.BorderBrush = Brushes.Red;
-                                        Password.BorderThickness = new Thickness(2);
-
-                                        LoginError.Content = "Het wachtwoord komt niet met deze gebruiker overeen.";
-                                        LoginError.UpdateLayout();
-                                        Password.UpdateLayout();
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                Username.BorderBrush = Brushes.Red;
-                                Username.BorderThickness = new Thickness(2);
-
-                                LoginError.Content = "Deze gebruikersnaam bestaat niet";
-                                LoginError.UpdateLayout();
-                                Username.UpdateLayout();
-                            }
-
-                        }
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Lijst is leeg");
-                }
-
+                Switcher.Switch(new DashboardView());
             }
-
+            
         }
-        
-   
 
     public void UtilizeState(object state)
         {
@@ -105,7 +43,7 @@ namespace BootRegistratieSysteem
         }
 
              
-            private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+    private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
             {
                 Regex regex = new Regex("[^0-9]+");
                 e.Handled = regex.IsMatch(e.Text);
