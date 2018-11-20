9278a5518bd117f71fbc850a6f585b5628984546
Omar Mazher <S1113954@student.windesheim.nl>
2018-11-20 14:42:08 +0100

    Update Registreren
    MVC updated

--- BootRegistratieSysteem/Views/Register.xaml.cs
+++ BootRegistratieSysteem/Views/Register.xaml.cs
@@ -15,6 +15,7 @@ using System.Text.RegularExpressions;
 using BootRegistratieSysteem.Views;
 using System.Globalization;
 using System.Net.Mail;
+using BootRegistratieSysteem.Controller;
 
 namespace BootRegistratieSysteem
 {
@@ -38,144 +39,17 @@ namespace BootRegistratieSysteem
         //Register member of user
         private void ButtonRegister(object sender, RoutedEventArgs e)
         {
-            bool validate = true;
-            bool valDate = true;
-            DateTime dt = new DateTime();
-            TextBox[] controls = { Firstname, Middlename, Lastname, City, Zipcode, Address, Phonenumber, Email, Day, Month, Year };
-
-            foreach (var item in controls)
+            if (RegisterController.Registreren(Firstname, Middlename, Lastname, City, Zipcode, Address, Phonenumber, Email, Day, Month, Year, Gender, Password, ConfirmPassword))
             {
-
-                item.BorderBrush = Brushes.Gray;
-                item.BorderThickness = new Thickness(1);
-
-
-                if (item.Text == "" && item.Name != "Middlename")
-                {
-                    item.BorderBrush = Brushes.Red;
-                    item.BorderThickness = new Thickness(2);
-                    validate = false;
-                    if (item.Name == "Day" || item.Name == "Month" || item.Name == "Year")
-                    {
-                        valDate = false;
-                    }
-                }
-                if (item.Name == "Firstname" || item.Name == "Lastname" || item.Name == "Middlename" || item.Name == "City")
-                {
-                    if (!IsAllLetters(item.Text))
-                    {
-                        item.BorderBrush = Brushes.Red;
-                        item.BorderThickness = new Thickness(2);
-                        item.UpdateLayout();
-                        validate = false;
-                    }
-                }
-            }
-            // update password layout 
-            Password.BorderBrush = Brushes.Gray;
-            Password.BorderThickness = new Thickness(1);
-            ConfirmPassword.BorderBrush = Brushes.Gray;
-            ConfirmPassword.BorderThickness = new Thickness(1);
-            // check if passwords are filled 
-            if (Password.Password == "")
-            {
-                Password.BorderBrush = Brushes.Red;
-                Password.BorderThickness = new Thickness(2);
-                validate = false;
-            }
-            if (ConfirmPassword.Password == "")
-            {
-                ConfirmPassword.BorderBrush = Brushes.Red;
-                ConfirmPassword.BorderThickness = new Thickness(2);
-                validate = false;
-            }
-
-            DataBaseController u = new DataBaseController(); // Get database
-
-            string savedPasswordHash = u.PasswordHash(Password.Password); // Hash password
-            string BirthdayText = $"{Day.Text}-{Month.Text}-{Year.Text}";
-
-            //localtime
-            DateTime DateTimeToday = DateTime.UtcNow.Date;
-            string DateToday = DateTimeToday.ToString("dd-MM-yyyy");
-
-            // validate date
-            if (valDate && ((int.Parse(Day.Text) > 31) || (int.Parse(Month.Text) > 12) || (int.Parse(Year.Text) > int.Parse(DateTime.Today.Year.ToString()))))
-            {
-                Day.BorderBrush = Brushes.Red;
-                Day.BorderThickness = new Thickness(2);
-                Day.UpdateLayout();
-                Month.BorderBrush = Brushes.Red;
-                Month.BorderThickness = new Thickness(2);
-                Month.UpdateLayout();
-                Year.BorderBrush = Brushes.Red;
-                Year.BorderThickness = new Thickness(2);
-                Year.UpdateLayout();
-
-                valDate = false;
-            }
-
-
-            int GenderID = int.Parse(((ComboBoxItem)this.Gender.SelectedItem).Tag.ToString());
-
-            // email validation
-            if (Email.Text != "" && !IsEmailValid(Email.Text))
-            {
-                validate = false;
-                Email.BorderBrush = Brushes.Red;
-                Email.BorderThickness = new Thickness(2);
-                Email.UpdateLayout();
-            }
-            
-
-            if (valDate && validate)
-            {
-                dt = DateTime.ParseExact(BirthdayText, "dd-MM-yyyy", CultureInfo.InvariantCulture);
-            }
-
-
-            // validate passwords
-            if (Password.Password == "")
-            {
-                Password.BorderBrush = Brushes.Red;
-                Password.BorderThickness = new Thickness(2);
-                Password.UpdateLayout();
-                validate = false;
-            }
-
-            if (ConfirmPassword.Password == "")
-            {
-                ConfirmPassword.BorderBrush = Brushes.Red;
-                ConfirmPassword.BorderThickness = new Thickness(2);
-                ConfirmPassword.UpdateLayout();
-                validate = false;
-            }
-
-            if (!Password.Password.Equals(ConfirmPassword.Password))
-            {
-                Password.BorderBrush = Brushes.Red;
-                Password.BorderThickness = new Thickness(2);
-                ConfirmPassword.BorderBrush = Brushes.Red;
-                ConfirmPassword.BorderThickness = new Thickness(2);
-                Password.UpdateLayout();
-                ConfirmPassword.UpdateLayout();
-                validate = false;
-            }
-
-            // add user to database
-            if (validate && valDate)
-            {
-
-                MessageBoxResult result = MessageBox.Show("UW ACCOUNT IS AANGEMAAKT!!!!");
-                u.Add_User(savedPasswordHash, Firstname.Text, Lastname.Text, Address.Text, Zipcode.Text, City.Text, Phonenumber.Text, Email.Text, GenderID, dt);
                 Switcher.Switch(new DashboardView());
-
             }
             else
             {
                 RegisterError.Content = "Controleer uw gegevens!";
                 RegisterError.UpdateLayout();
             }
+            
+            
 
         }
         // check if there are no numbers in inputbox
@@ -198,20 +72,6 @@ namespace BootRegistratieSysteem
             e.Handled = regex.IsMatch(e.Text);
         }
 
-        public bool IsEmailValid(string emailaddress)
-        {
-            try
-            {
-
-                MailAddress m = new MailAddress(emailaddress);
-
-                return true;
-            }
-            catch (FormatException)
-            {
-                return false;
-            }
-        }
 
         // ISwitchable utilizes this
         public void UtilizeState(object state)
